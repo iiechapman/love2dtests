fb29b86adb230792c6c3ec76a6daae7bf6610a36
Evan <iiechapman@gmail.com>
2014-12-28 22:02:47 -0500

    adding new main lua file

---
+++ main.lua
@@ -0,0 +1,11 @@
+function love.load()
+	love.graphics.setNewFont(12)
+	love.graphics.setColor(0,0,0)
+	love.graphics.setBackgroundColor(255,255,255)
+end
+
+function love.draw()
+	love.graphics.print("Hello World",400,300)
+ 	love.graphics.print("Hello World Two",400,200)
+end
+
